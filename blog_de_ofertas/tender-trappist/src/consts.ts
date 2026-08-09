5a8f5c01bf434a9097d539b03112f49546312fed
NikezinW1 <nikezinw1@users.noreply.github.com>
2026-08-09 00:10:35 -0300

    refactor(ui): ajustar identidade visual e remover boilerplate generico

--- blog_de_ofertas/tender-trappist/src/consts.ts
+++ blog_de_ofertas/tender-trappist/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tech Oculta';
+export const SITE_DESCRIPTION = 'O submundo do hardware e da tecnologia. Análises sinceras e ofertas.';
