dcc7ad51fcba1057e3f88f4d7a3c40915e9765cd
NikezinW1 <nikezinw1@users.noreply.github.com>
2026-08-09 02:40:06 -0300

    feat(branding): migrar projeto para a marca Nikezin Indica

--- blog_de_ofertas/tender-trappist/src/consts.ts
+++ blog_de_ofertas/tender-trappist/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tech Oculta';
+export const SITE_TITLE = 'Nikezin Indica';
 export const SITE_DESCRIPTION = 'O submundo do hardware e da tecnologia. Análises sinceras e ofertas.';
